125174dd9d600a72894e8cad1207aeff50a24a05
Adam Pluguez <70963681+adaPlu@users.noreply.github.com>
2026-08-24 08:43:13 -0700

    refactor: centralize game save and replay version constants

---
+++ Assets/ClickDungeon/Application/Versioning/GameVersionInfo.cs
@@ -0,0 +1,10 @@
+namespace ClickDungeon.Application.Versioning
+{
+    public static class GameVersionInfo
+    {
+        public const string GameVersion="0.2.0";
+        public const int SaveSchemaVersion=2;
+        public const int SimulationVersion=2;
+        public const int ContentRevision=2;
+    }
+}
