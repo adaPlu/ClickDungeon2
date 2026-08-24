31702b1910f705138945599a25940e8bcc839191
Adam Pluguez <70963681+adaPlu@users.noreply.github.com>
2026-08-24 08:45:37 -0700

    fix(versioning): centralize replay versions

--- Assets/ClickDungeon/Application/Replay/ReplayEnvelope.cs
+++ Assets/ClickDungeon/Application/Replay/ReplayEnvelope.cs
@@ -1,13 +1,14 @@
 using System;
 using System.Collections.Generic;
+using ClickDungeon.Application.Versioning;
 
 namespace ClickDungeon.Application.Replay
 {
     [Serializable]
     public sealed class ReplayEnvelope
     {
-        public int SimulationVersion = 2;
-        public int ContentRevision = 2;
+        public int SimulationVersion = GameVersionInfo.SimulationVersion;
+        public int ContentRevision = GameVersionInfo.ContentRevision;
         public uint RootSeed;
         public string HeroClassId = "Knight";
         public List<string> UnlockedAbilityIds = new List<string>();
