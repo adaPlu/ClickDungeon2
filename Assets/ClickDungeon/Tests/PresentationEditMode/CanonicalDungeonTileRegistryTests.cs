4a3aacea865d935bc03a93ac0700e50a0967f2ec
Adam Pluguez <70963681+adaPlu@users.noreply.github.com>
2026-09-09 22:34:56 -0700

    test: cover canonical tile filename mapping

--- Assets/ClickDungeon/Tests/PresentationEditMode/CanonicalDungeonTileRegistryTests.cs
+++ Assets/ClickDungeon/Tests/PresentationEditMode/CanonicalDungeonTileRegistryTests.cs
@@ -35,5 +35,34 @@ namespace ClickDungeon.Tests.PresentationEditMode
             Assert.That(ids, Does.Contain("tile.door.open"));
             Assert.That(ids, Does.Contain("tile.torch"));
         }
+
+        [TestCase("tile_floor_stone", "tile.floor.stone")]
+        [TestCase("tile_floor_cracked", "tile.floor.cracked")]
+        [TestCase("tile_floor_moss", "tile.floor.moss")]
+        [TestCase("tile_water", "tile.water")]
+        [TestCase("tile_lava", "tile.lava")]
+        [TestCase("tile_shadow", "tile.shadow")]
+        [TestCase("tile_trap_pit", "tile.trap.pit")]
+        [TestCase("tile_trap_bomb", "tile.trap.bomb")]
+        [TestCase("tile_trap_spike", "tile.trap.spike")]
+        [TestCase("tile_pressure_plate", "tile.pressure_plate")]
+        [TestCase("tile_teleport", "tile.teleport")]
+        [TestCase("tile_fountain_heal", "tile.fountain.heal")]
+        [TestCase("tile_stair_up", "tile.stair.up")]
+        [TestCase("tile_stair_up_locked", "tile.stair.up.locked")]
+        [TestCase("tile_stair_down", "tile.stair.down")]
+        [TestCase("tile_stair_down_locked", "tile.stair.down.locked")]
+        [TestCase("tile_wall", "tile.wall")]
+        [TestCase("tile_wall_corner", "tile.wall.corner")]
+        [TestCase("tile_key", "tile.key")]
+        [TestCase("tile_chest_closed", "tile.chest.closed")]
+        [TestCase("tile_chest_open", "tile.chest.open")]
+        [TestCase("tile_door_locked", "tile.door.locked")]
+        [TestCase("tile_door_open", "tile.door.open")]
+        [TestCase("tile_torch", "tile.torch")]
+        public void CanonicalTileRuntimeFilenamesMapToCanonicalIds(string file, string expected)
+        {
+            Assert.That(PresentationAssetIdMapper.SpriteId(file), Is.EqualTo(expected));
+        }
     }
 }
